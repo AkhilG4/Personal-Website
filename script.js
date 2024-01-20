192bd3a9d8e650e25ef6dce924f7a2532bad2b55
Akhil <88569103+RapidCow1@users.noreply.github.com>
2024-01-20 17:42:40 +0800

    Prevent constant animation

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
     sections.forEach(section => observer.observe(section));
 });
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const sections = document.querySelectorAll('section');
+
+//     const observer = new IntersectionObserver((entries) => {
+//         entries.forEach((entry) => {
+//             if (entry.isIntersecting) {
+//                 entry.target.classList.add('show');
+//             }
+//         });
+//     }, { threshold: 0});
+
+//     sections.forEach(section => observer.observe(section));
+// });
